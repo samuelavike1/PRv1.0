3f7a07ef83ffdca521bc939f938a1a61a55b8b1d
samuelavike1 <samuelavike@gmail.com>
2024-04-08 12:27:26 +0000

    deployed update

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,12 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+
 
 
 Route::middleware(['auth','verified'])->group(function (){
 //    Route::get('/dashboard', fn () =>Inertia::render('Dashboard'))->name('dashboard');
-    Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
     Route::resource('project', \App\Http\Controllers\ProjectController::class);
     Route::resource('task', \App\Http\Controllers\TaskController::class);
